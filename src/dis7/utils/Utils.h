984e4da484a4cf23b3e84795469c35e52f32faa2
GigiElt <luigi.salerno@elt.it>
2024-11-08 12:11:53 +0100

    docs: add comments in Utils class

--- src/dis7/utils/Utils.h
+++ src/dis7/utils/Utils.h
@@ -8,10 +8,35 @@ const double PI = 3.14159265358979;
 class Utils
 {
 public:
+    /**
+     * @brief getOrientationFromEuler
+     * @param lat should be expressed in radians
+     * @param lon should be expressed in radians
+     * @param psi shall follow Tait-Bryan angle convention in the XYZ
+     * @param theta shall follow Tait-Bryan angle convention in the XYZ
+     * @return 
+     */
     static double getOrientationFromEuler(double lat, double lon, double psi, double theta);
 
+    /**
+     * @brief getPitchFromEuler
+     * @param lat should be expressed in radians
+     * @param lon should be expressed in radians
+     * @param psi shall follow Tait-Bryan angle convention in the XYZ
+     * @param theta shall follow Tait-Bryan angle convention in the XYZ
+     * @return 
+     */
     static double getPitchFromEuler(double lat, double lon, double psi, double theta);
 
+    /**
+     * @brief getRollFromEuler
+     * @param lat should be expressed in radians
+     * @param lon should be expressed in radians
+     * @param psi shall follow Tait-Bryan angle convention in the XYZ
+     * @param theta shall follow Tait-Bryan angle convention in the XYZ
+     * @param phi shall follow Tait-Bryan angle convention in the XYZ
+     * @return 
+     */
     static double getRollFromEuler(double lat, double lon, double psi, double theta, double phi);
 
     static double radToDeg(double rad) { return rad * (180 / PI); };
